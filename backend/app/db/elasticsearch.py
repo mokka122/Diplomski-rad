b5a328f309dcf0032a2f4060b327c2790257db29
mokka122 <monikalakic12@gmail.com>
2026-08-23 00:19:27 +0200

    Elasticsearch update

--- backend/app/db/elasticsearch.py
+++ backend/app/db/elasticsearch.py
@@ -12,7 +12,29 @@ ELASTICSEARCH_URL = os.getenv(
     "http://localhost:9200",
 )
 
+ELASTICSEARCH_USERNAME = os.getenv(
+    "ELASTICSEARCH_USERNAME"
+)
+
+ELASTICSEARCH_PASSWORD = os.getenv(
+    "ELASTICSEARCH_PASSWORD"
+)
+
+
+client_kwargs = {}
+
+
+if (
+    ELASTICSEARCH_USERNAME
+    and ELASTICSEARCH_PASSWORD
+):
+    client_kwargs["basic_auth"] = (
+        ELASTICSEARCH_USERNAME,
+        ELASTICSEARCH_PASSWORD,
+    )
+
 
 elasticsearch_client = AsyncElasticsearch(
     ELASTICSEARCH_URL,
+    **client_kwargs,
 )
